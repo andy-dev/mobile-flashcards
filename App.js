205df36a11d0dda20f7b215e8dc1f786730c4604
andres macedo <andres.macedo@gmail.com>
2018-01-26 13:33:21 -0600

    using old project, that uses oldever versions of react-native that do not break simulator

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
-import AddEntry from './components/AddEntry'
+import { View, Text } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
@@ -9,53 +8,12 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View>
-          <AddEntry />
+        <View style={{flex:1}}>
+          <Text>Hello</Text>
         </View>
       </Provider>
     )
   }
 }
 
-//
-// state = {
-//   input: '@andy-dev',
-//   showInput: false,
-// }
-//
-// handleToggleSwitch = (state) => {
-//   this.setState(() => ({
-//     showInput : !state.showInput,
-//   }))
-// }
-//
-// handleTextChange = (input) => {
-//   this.setState(()=>({
-//     input
-//   }))
-// }
-//
-//
-// renderItem = ({item}) =>{
-//   return <Review {...item}/>
-// }
 
-
-// const {input, showInput} = this.state
-// const reviews  = getReviews()
-
-
-{/*{showInput === true && (*/}
-{/*<TextInput value {input} onChangeText={this.handleTextChange} />*/}
-{/*)}*/}
-
-/*<AddEntry/>*/
-/*<FlatList*/
-/*data={reviews}*/
-/*renderItem={this.renderItem}*/
-/*/>*/
-
-
-// {reviews.map(({name, text, avatar})=>
-//   <Review key={name} name={name} avatar={avatar}/>
-// )}
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
-import { white } from './colors'
+import { red, orange, blue, lightPurp, pink, white } from './colors'
 
 export function getDailyReminderValue () {
   return {
     today: "👋 Don't forget to log your data today!"
   }
 }
+
+const styles = StyleSheet.create({
+  iconContainer: {
+    padding: 5,
+    borderRadius: 8,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 20
+  },
+})
 
 export function getMetricMetaInfo (metric) {
   const info = {
@@ -19,12 +31,12 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
-              <MaterialIcons
-                name='directions-run'
-                color={white}
-                size={35}
-              />
+          <View style={[styles.iconContainer, {backgroundColor: red}]}>
+            <MaterialIcons
+              name='directions-run'
+              color={white}
+              size={35}
+            />
           </View>
         )
       }
@@ -37,12 +49,12 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
-              <MaterialCommunityIcons
-                name='bike'
-                color={white}
-                size={32}
-              />
+          <View style={[styles.iconContainer, {backgroundColor: orange}]}>
+            <MaterialCommunityIcons
+              name='bike'
+              color={white}
+              size={32}
+            />
           </View>
         )
       }
@@ -55,12 +67,12 @@ export function getMetricMetaInfo (metric) {
       type: 'steppers',
       getIcon() {
         return (
-          <View>
-              <MaterialCommunityIcons
-                name='swim'
-                color={white}
-                size={35}
-              />
+          <View style={[styles.iconContainer, {backgroundColor: blue}]}>
+            <MaterialCommunityIcons
+              name='swim'
+              color={white}
+              size={35}
+            />
           </View>
         )
       }
@@ -73,12 +85,12 @@ export function getMetricMetaInfo (metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View>
-              <FontAwesome
-                name='bed'
-                color={white}
-                size={30}
-              />
+          <View style={[styles.iconContainer, {backgroundColor: lightPurp}]}>
+            <FontAwesome
+              name='bed'
+              color={white}
+              size={30}
+            />
           </View>
         )
       }
@@ -91,12 +103,12 @@ export function getMetricMetaInfo (metric) {
       type: 'slider',
       getIcon() {
         return (
-          <View>
-              <MaterialCommunityIcons
-                name='food'
-                color={white}
-                size={35}
-              />
+          <View style={[styles.iconContainer, {backgroundColor: pink}]}>
+            <MaterialCommunityIcons
+              name='food'
+              color={white}
+              size={35}
+            />
           </View>
         )
       }
@@ -107,6 +119,7 @@ export function getMetricMetaInfo (metric) {
     ? info
     : info[metric]
 }
+
 
 export function isBetween (num, x, y) {
   if (num >= x && num <= y) {
