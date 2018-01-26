66f5e5689a4816197991f8c8c0e345cd11411413
andres macedo <andres.macedo@gmail.com>
2018-01-26 14:04:15 -0600

    added basic setup to start navigation in app using tabnavigator to change between decks and new decks

--- App.js
+++ App.js
@@ -1,15 +1,59 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import { Constants } from 'expo'
+import { purple } from "./utils/colors";
+import { TabNavigator, StackNavigator, DrawerNavigator } from 'react-navigation'
+import { FontAwesome, Ionicons} from '@expo/vector-icons'
+
+function FlashCardsStatusBar({backgroundColor, ...props}){
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
+
+function DeckList(){
+  return(
+    <View>
+      <Text>DeckList</Text>
+    </View>
+  )
+}
+
+function NewDeck(){
+  return(
+    <View>
+      <Text>New Deck</Text>
+    </View>
+  )
+}
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions:{
+      tabBarIcon: ()=> <FontAwesome name='home' size={30} color='black' />
+    }
+  },
+  NewDeck:{
+    screen: NewDeck,
+    navigationOptions:{
+      tabBarIcon: ()=> <Ionicons name='ios-add' size={30} color='black' />
+    }
+  }
+});
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex:1}}>
-          <Text>Hello</Text>
+          <FlashCardsStatusBar backgroundColor={purple} barStyle='light-content'/>
+            <Tabs/>
         </View>
       </Provider>
     )
